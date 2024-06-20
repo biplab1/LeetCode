5e5066eba5dcaf088dc70befd3e2d26af9928d7b
biplab1 <biplabdutta27@gmail.com>
2024-06-20 14:02:06 +0530

    Create ContainerWithMostWater.java

--- code/ContainerWithMostWater/ContainerWithMostWater.java
+++ code/ContainerWithMostWater/ContainerWithMostWater.java
@@ -1,3 +1,6 @@
+/*
+ * Method 1.0:
+ */
 class Solution {
     public int maxArea(int[] height) {
         int left = 0;
@@ -21,3 +24,31 @@ class Solution {
         return maxArea;
     }
 }
+
+/*
+ * Method 1.1:
+ */
+/*
+ * class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+
+        int maxArea = 0;
+
+        while (left < right) {
+
+            int base = right - left;
+            int top = Math.min(height[left], height[right]);
+            maxArea = Math.max(base * top, maxArea);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
+ */
