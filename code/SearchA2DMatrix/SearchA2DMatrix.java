3297bb18dd1390fdafc155d9c22662f3ceda1311
biplab1 <biplabdutta27@gmail.com>
2024-06-28 15:40:01 +0530

    Create SearchA2DMatrix.java

--- code/SearchA2DMatrix/SearchA2DMatrix.java
+++ code/SearchA2DMatrix/SearchA2DMatrix.java
@@ -1,3 +1,7 @@
+/*
+ * Method 1.0:
+ */
+
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int n = matrix.length;
@@ -22,3 +26,30 @@ class Solution {
         return false;
     }
 }
+
+/*
+ * Method 1.1:
+ */
+/*
+ * class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int i = 0;
+        int j = m - 1;
+
+        while (i < n && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+
+        return false;
+    }
+}
+ */
