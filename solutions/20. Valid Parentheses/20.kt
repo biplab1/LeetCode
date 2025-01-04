6307d754eebef60748559cb62fadf10bf41727cc
biplab1 <biplabdutta27@gmail.com>
2025-01-04 12:56:11 +0530

    LeetCode solutions

---
+++ solutions/20. Valid Parentheses/20.kt
@@ -0,0 +1,23 @@
+class Solution {
+    fun isValid(s: String): Boolean {
+        val stack = Stack<Char>()
+
+        for (c in s) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c)
+            } else {
+                if (stack.isEmpty()) return false
+                else {
+                    if ((c == ')' && stack.peek() == '(') ||
+                        (c == '}' && stack.peek() == '{') ||
+                        (c == ']' && stack.peek() == '[')) {
+                        stack.pop()
+                    }
+                    else return false
+                }
+            }
+        }
+
+        return stack.isEmpty()
+    }
+}
