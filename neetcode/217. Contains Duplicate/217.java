5a22173764b94af413fb0add78e4e742c01dbeaf
biplab1 <biplabdutta27@gmail.com>
2025-02-06 23:11:40 +0530

    neetcode DSA solutions

---
+++ neetcode/217. Contains Duplicate/217.java
@@ -0,0 +1,18 @@
+/*
+Method 1.0:
+Intution: Build a set, if a number appears more than once, adding to set will return false
+TC: O(N)
+SC: O(N), it can be done in O(1) also without using a set
+ */
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (!set.add(num)) return true;
+        }
+
+        return false;
+    }
+}
