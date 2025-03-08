e4d9a9f390ea688e675444be1b8348f41bce8dea
biplab1 <biplabdutta27@gmail.com>
2025-03-08 15:13:35 +0530

    neetcode DSA solutions

---
+++ neetcode/572. Subtree of Another Tree/572.kt
@@ -0,0 +1,28 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun isSubtree(root: TreeNode?, subRoot: TreeNode?): Boolean {
+        if (root == null) return false
+
+        if (isSametree(root, subRoot)) return true
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
+    }
+
+    fun isSametree(s: TreeNode?, t: TreeNode?): Boolean {
+        if (s == null && t == null) return true
+        if (s == null || t == null) return false
+
+        if (s.`val` != t.`val`) return false
+
+        return isSametree(s.left, t.left) && isSametree(s.right, t.right)
+    }
+}
